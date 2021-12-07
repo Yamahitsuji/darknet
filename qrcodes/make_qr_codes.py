ddbbde5cd1cbfceceb52aafc4d767da1f72439ff
Yamahitsuji <goat06131004@gmail.com>
2021-12-07 18:09:53 +0900

    fix: qrcode creation

--- qrcodes/make_qr_codes.py
+++ qrcodes/make_qr_codes.py
@@ -4,13 +4,13 @@ import qrcode
 
 def run():
     for i in range(5):
-        randID = random.randint(1, 50000)
-        img = qrcode.make(randID, version=1, box_size=12,
+        rand_id = random.randint(1, 50000)
+        img = qrcode.make(str(rand_id) + '_H', version=1, box_size=12,
                           error_correction=qrcode.constants.ERROR_CORRECT_H).convert('RGB')
-        img.save('./out/{}_H.png'.format(randID))
-        img = qrcode.make(randID, version=1, box_size=12,
+        img.save('./out/{}_H.png'.format(rand_id))
+        img = qrcode.make(str(rand_id) + '_L', version=1, box_size=12,
                           error_correction=qrcode.constants.ERROR_CORRECT_L).convert('RGB')
-        img.save('./out/{}_L.png'.format(randID))
+        img.save('./out/{}_L.png'.format(rand_id))
 
 
 if __name__ == '__main__':
